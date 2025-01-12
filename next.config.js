06be951f51a3fc8075c83229822e79288bd4d69a
Raihan Rafid <raihanrfd1@gmail.com>
2025-01-12 22:52:00 +0700

    feat: finish all pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "https://github.com/rrafids/bilqis-raihan-wedding",
   images: {
     domains: ['raw.githubusercontent.com'],
   },
