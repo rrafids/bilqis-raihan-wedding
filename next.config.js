b19f738076fe42e44baf7b6e0c73198b92459a11
Raihan Rafid <raihanrfd1@gmail.com>
2025-01-17 19:43:41 +0700

    feat: finish all pages

--- next.config.js
+++ next.config.js
@@ -6,16 +6,16 @@ const nextConfig = {
     domains: ['raw.githubusercontent.com'],
     unoptimized: true, // Disable Image Optimization
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore 'self' references for the server-side build
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        self: false, // Avoid 'self' reference in server-side code
-      };
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     // Ignore 'self' references for the server-side build
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback,
+  //       self: false, // Avoid 'self' reference in server-side code
+  //     };
+  //   }
+  //   return config;
+  // },
 }
 
 module.exports = nextConfig
